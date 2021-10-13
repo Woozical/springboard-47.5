e5c7981e0a19d6a34ed9daf437260ff0919143d4
Mark <mark@example.dev>
2021-10-13 15:08:33 -0700

    push() and pop()

--- stack.js
+++ stack.js
@@ -1,9 +1,9 @@
 /** Node: node for a stack. */
 
 class Node {
-  constructor(val) {
+  constructor(val, next=null) {
     this.val = val;
-    this.next = null;
+    this.next = next;
   }
 }
 
@@ -17,17 +17,26 @@ class Stack {
     this.size = 0;
   }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+  /** push(val): add new value to top of the stack. Returns undefined. */
 
   push(val) {
-
+    const next = this.first ? this.first : null;
+    const newNode = new Node(val, next);
+    this.first = newNode;
+    if (!this.last) this.last = this.first;
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (!this.size) throw new RangeError('Cannot pop from empty stack');
+    const top = this.first;
+    this.first = top.next;
+    if (!this.first) this.last = null;
+    this.size--;
+    return top.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
